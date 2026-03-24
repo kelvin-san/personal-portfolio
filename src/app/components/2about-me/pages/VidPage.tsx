64f6aeae392b7a41357e5cf71290a49570ca84c4
kelvin-san <kelvinsanrib@gmail.com>
2026-03-24 14:15:06 -0300

    refactor: move YouTube iframe URL to env variable

--- src/app/components/2about-me/pages/VidPage.tsx
+++ src/app/components/2about-me/pages/VidPage.tsx
@@ -1,5 +1,7 @@
 import styles from "./vidPage.module.css";
 
+const videoURL = process.env.NEXT_PUBLIC_VIDEO_URL;
+
 export default function VidPage() {
   return (
     <div className={`${styles.container}`}>
@@ -7,7 +9,7 @@ export default function VidPage() {
         <div className={`${styles.frameVideo}`}>
           <iframe
             width="840" height="472"
-            src="https://www.youtube.com/embed/ScMzIvxBSi4?si=2TImH9n0F4AsR6u0"
+            src={videoURL}
             title="YouTube video player"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen;"
             referrerPolicy="strict-origin-when-cross-origin">
